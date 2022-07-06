483cd774d41665f52c72914082e9a04bfbed93fd
Facundo Martín Scasso <facundomartinscasso@gmail.com>
2022-07-06 14:54:10 -0300

    add CORSMiddleware and add conda requirements file

--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backbone.routers import post, user, auth, vote
 
 # models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-my_posts = [
-    {
-        "title": "Hello World",
-        "content": "My first post",
-        "published": True,
-        "id": 1,
-    },
-]
-
-
-def find_index_post(id):
-    return next(i for i, p in enumerate(my_posts) if p["id"] == id)
-
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+origins = ["*"]  # TODO: PLEASE DO NOT LEAVE THIS LIKE THIS IN A PRODUCTION ENV!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
