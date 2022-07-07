5344d288c4a396174230120af2a318f68d323ba2
Facundo Martín Scasso <facundomartinscasso@gmail.com>
2022-07-07 17:00:34 -0300

    erased relative imports so that the app works again

--- app/backbone/database.py
+++ app/backbone/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import settings
+from app.backbone.config import settings
 
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip-address/hostname>/<database_name>"
